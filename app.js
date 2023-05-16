70f9afdd219ff3d0a582e3f9390ef1b31c38914b
Shazia456 <128918032+Shazia456@users.noreply.github.com>
2023-05-16 08:21:55 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+var myAge;
+myAge = "I am 21 years old";
+alert(myAge);
+
+
+//    Ques#2
+var vist = "You have visited this side N times ";
+alert(vist);
+
+
+//    Ques#3
+// var birthYear =("My birth  is 2001 <br> Data type of my declared variable is number");
+// document.write(birthYear)
+// Ques#4
+var visitorsName = "Mark"
+var productTitle ="T-shirts"
+var Quantity = 5;
+document.write( visitorsName + "ordered" + Quantity + productTitle + "on XYZ store" )
+
+
+
+
+
+
+
