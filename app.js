47ebeca1da637f17eee2fea6dff5e01d9338f239
Shazia456 <128918032+Shazia456@users.noreply.github.com>
2023-05-16 08:23:04 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
-var myAge;
-myAge = "I am 21 years old";
-alert(myAge);
+// //  Ques#1
+// let name = ("shazia"); let age = 21; let favColor = ("pink");
 
 
-//    Ques#2
-var vist = "You have visited this side N times ";
-alert(vist);
+// // Ques#2 legal variables
+// let myName = "shazia";
+// let myAge = "21 years";
+// let favColor = "pink";
+// let weight = "56kg";
+// let $income = 15000;
+
+// // illegal variables
+
+// let my Name ="shazia";
+// let house# = 124;
+// let 3favColors ="pink,blue,green";
+// let weight + height = 56;
+// let alert = "hello world";
 
 
-//    Ques#3
-// var birthYear =("My birth  is 2001 <br> Data type of my declared variable is number");
-// document.write(birthYear)
-// Ques#4
-var visitorsName = "Mark"
-var productTitle ="T-shirts"
-var Quantity = 5;
-document.write( visitorsName + "ordered" + Quantity + productTitle + "on XYZ store" )
-
-
-
+// // Ques#3
+document.write("<b> Rules for naming JS variables");
+document.write( "<br> <b> Variables name can only contain letters,numbers,$ and _. For example $my_1st variable");
+document.write("<br> variable must begin with a $sign , underscore and letter. For example $name,_name or name");
+document.write("<br> Variable names are case sensitive");
+document.write("<br> Variable names should not be JS keywords");
 
 
 
